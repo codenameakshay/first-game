d7b491cbd8e4a64a6b79234269b3ec84c3d823bd
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-07-31 01:56:13 +0530

    added speed

---
+++ Assets/Scripts/Speed.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Speed : MonoBehaviour
+{
+    public Rigidbody player;
+    public Text speedText;
+    // Update is called once per frame
+    void Update()
+    {
+        speedText.text = player.velocity.z.ToString("0") + " m/s";
+    }
+}
