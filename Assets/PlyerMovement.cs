da47305d57085c6d2c3274e40282dcdb1242dbed
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-07-30 17:04:38 +0530

    added force

---
+++ Assets/PlyerMovement.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PlyerMovement : MonoBehaviour {
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        rb.AddForce (0, 0, 500 * Time.deltaTime);
+    }
+}
