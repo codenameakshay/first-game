f21981332233856cd09261d0ec4bfdb344791827
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-07-30 18:08:50 +0530

    simple controls, game level 1 done

--- Assets/PlyerMovement.cs
+++ Assets/PlyerMovement.cs
@@ -4,7 +4,7 @@ public class PlyerMovement : MonoBehaviour
 {
     public Rigidbody rb;
     public float forwardForce = 1000f;
-    public float horizontalForce = 500f;
+    public float horizontalForce = 1000f;
 
     // Update is called once per frame
     void FixedUpdate()
@@ -12,11 +12,15 @@ public class PlyerMovement : MonoBehaviour
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
         if (Input.GetKey("d"))
         {
-            rb.AddForce(horizontalForce * Time.deltaTime, 0, 0);
+            rb.AddForce(horizontalForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-horizontalForce * Time.deltaTime, 0, 0);
+            rb.AddForce(-horizontalForce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 2 * horizontalForce * Time.deltaTime, 0,ForceMode.VelocityChange);
         }
     }
 }
