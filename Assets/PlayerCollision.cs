2fe86451aca44d478b6b65b57bc152b4e709403c
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-07-30 17:26:27 +0530

    added zero friction and collision script

--- Assets/PlayerCollision.cs
+++ Assets/PlayerCollision.cs
@@ -1,18 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnCollisionEnter(Collision other) {
+        Debug.Log("Hello");
     }
 }
