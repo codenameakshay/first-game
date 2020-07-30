80d262d6cf773a010b6a43682217b9ca335d5d8b
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-07-30 17:17:26 +0530

    player movement, camera movement added

--- Assets/PlyerMovement.cs
+++ Assets/PlyerMovement.cs
@@ -1,10 +1,22 @@
 ﻿using UnityEngine;
 
-public class PlyerMovement : MonoBehaviour {
+public class PlyerMovement : MonoBehaviour
+{
     public Rigidbody rb;
+    public float forwardForce = 500f;
+    public float horizontalForce = 500f;
 
     // Update is called once per frame
-    void FixedUpdate () {
-        rb.AddForce (0, 0, 500 * Time.deltaTime);
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(horizontalForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-horizontalForce * Time.deltaTime, 0, 0);
+        }
     }
 }
