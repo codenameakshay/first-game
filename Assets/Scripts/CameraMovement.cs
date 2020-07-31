f89a8afa8c6442df378e76290fcb3ebbe0090815
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-07-31 21:12:54 +0530

    Created smooth camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -3,10 +3,15 @@
 public class CameraMovement : MonoBehaviour
 {
     public Transform player;
+
+    public float smoothSpeed = 10f;
     public Vector3 offset;
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = player.position + offset;
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position,desiredPosition,smoothSpeed*Time.deltaTime);
+        transform.position = new Vector3(smoothPosition.x,smoothPosition.y,desiredPosition.z);
+        transform.LookAt(player);
     }
 }
