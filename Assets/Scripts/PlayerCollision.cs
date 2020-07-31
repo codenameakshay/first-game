d8be0564c4203c5686d28e6ce946e8234937f0a7
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-08-01 00:06:45 +0530

    Added red filter when obstacle hit

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,14 +1,32 @@
 ﻿using UnityEngine;
-
+using UnityEngine.Rendering.PostProcessing;
 public class PlayerCollision : MonoBehaviour
 {
     public PlyerMovement playerMovement;
+    public GameObject cameraFX;
+    public int health = 3;
+    ColorGrading colorGradingLayer = null;
     void OnCollisionEnter(Collision other)
     {
         if (other.collider.tag == "Obstacle")
         {
-            playerMovement.enabled = false;
-            FindObjectOfType<GameManager>().EndGame();
+            if (health == 1)
+            {
+                playerMovement.enabled = false;
+                FindObjectOfType<GameManager>().EndGame();
+            }
+            else if (health != 1)
+            {
+                health = health - 1;
+
+            }
+            PostProcessVolume volume = cameraFX.GetComponent<PostProcessVolume>();
+            volume.profile.TryGetSettings(out colorGradingLayer);
+            colorGradingLayer.enabled.value = true;
+            float originalRed = colorGradingLayer.mixerRedOutRedIn.value;
+            colorGradingLayer.mixerRedOutRedIn.value = originalRed + 50f;
+            Debug.Log(health);
         }
+
     }
 }
